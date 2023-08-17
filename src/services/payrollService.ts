2d4204ac3f7f5a68900b74f4c96bc7252dfd0aaa
devHDK <hyundo91082@gmail.com>
2023-08-17 18:42:53 +0900

    web | 예약 생성 seq 버그 수정

--- src/services/payrollService.ts
+++ src/services/payrollService.ts
@@ -8,9 +8,10 @@ import {findBetweenReservationWithTrainerId} from '../models/reservation'
 async function findAllWithMonth(req: IPayrollFindAll): Promise<IPayrollResponse> {
   try {
     const {trainerId, franchiseId, startDate} = req
-    const endDate = moment(startDate).utc().add(1, 'month').format('YYYY-MM-DD')
-    const lastMonthStartDate = moment(startDate).utc().subtract(1, 'month').format('YYYY-MM-DD')
-    const lastMonthEndDate = moment(lastMonthStartDate).utc().add(1, 'month').format('YYYY-MM-DD')
+    const endDate = moment(startDate).add(1, 'month').format('YYYY-MM-DD')
+    const lastMonthStartDate = moment(startDate).subtract(1, 'month').format('YYYY-MM-DD')
+    const lastMonthEndDate = moment(lastMonthStartDate).add(1, 'month').format('YYYY-MM-DD')
+
     //baseWage 및 percentage
     const wageInfo = await Trainer.findTrainerWageInfo({trainerId, franchiseId})
     //tickets
