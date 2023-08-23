2aa09baf28306e94fbc1c9dd880947a295296747
devHDK <hyundo91082@gmail.com>
2023-08-23 23:15:55 +0900

    app | Modify sign in  trainer device list에 맞춰서 처리

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -28,18 +28,31 @@ async function signIn(options: {
       const existDevices = await UserDevice.findAllWithUserId(user.id)
       const forbiddenDeviceList = await Trainer.findDeviceList()
 
+      if (!forbiddenDeviceList.includes(user.deviceId) && token) {
+        await User.updateOne({id: user.id, deviceId, platform}, connection)
+        await UserDevice.upsertOne({userId: user.id, platform, deviceId, token}, connection)
+        await UserDevice.updateOne({userId: user.id, platform, deviceId, isNotification: true}, connection)
+      }
+
       if (existDevices.length > 1) {
+        //기존 device list가 2개 이상이면 trainer device 전부 삭제
         for (const device of existDevices) {
           if (forbiddenDeviceList.includes(device.deviceId)) {
             await UserDevice.deleteOne(device.deviceId, user.id, connection)
           }
         }
-      }
-
-      if (token) {
-        await User.updateOne({id: user.id, deviceId, platform}, connection)
-        await UserDevice.upsertOne({userId: user.id, platform, deviceId, token}, connection)
-        await UserDevice.updateOne({userId: user.id, platform, deviceId, isNotification: true}, connection)
+      } else if (existDevices.length === 1) {
+        if (forbiddenDeviceList.includes(existDevices[0].deviceId) && !forbiddenDeviceList.includes(user.deviceId)) {
+          // forbiddenDeviceList 에 입력된 deviceId가 포함되있지 않고, 기존 deviceId가 trainer deviceId일 경우
+          await UserDevice.deleteOne(existDevices[0].deviceId, user.id, connection)
+        }
+      } else if (existDevices.length < 1) {
+        //리스트에 아무것도 없을땐 무조건 추가
+        if (token) {
+          await User.updateOne({id: user.id, deviceId, platform}, connection)
+          await UserDevice.upsertOne({userId: user.id, platform, deviceId, token}, connection)
+          await UserDevice.updateOne({userId: user.id, platform, deviceId, isNotification: true}, connection)
+        }
       }
 
       delete user.password
