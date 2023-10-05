786b31365c76eb76cff553fa12cf25f4a24c01fc
RyanJang <hiship56@gmail.com>
2023-10-05 14:13:32 +0900

    web | 댓글 CRUD 추가

---
+++ src/api/routes/web/comments/index.ts
@@ -0,0 +1,53 @@
+import {ApiRouter} from '../../default'
+import * as ctrl from './comments-ctrl'
+
+const postComments = new ApiRouter({
+  name: '',
+  method: 'post',
+  summary: 'comments 생성',
+  tags: ['Comment'],
+  schema: 'requests/web/comments/PostComments',
+  responses: {
+    200: {description: 'Success'}
+  },
+  handler: ctrl.postComments
+})
+
+const getComments = new ApiRouter({
+  name: '',
+  method: 'get',
+  summary: 'comments 목록 조회',
+  tags: ['Comment'],
+  schema: 'requests/web/comments/GetComments',
+  responses: {
+    200: {schema: 'responses/web/comments/GetComments'}
+  },
+  handler: ctrl.getComments
+})
+
+const putCommentsWithId = new ApiRouter({
+  name: ':id',
+  paths: ['common/IdPath'],
+  method: 'put',
+  summary: 'comments 수정',
+  tags: ['Comment'],
+  schema: 'requests/web/comments/PutCommentsWithId',
+  responses: {
+    200: {description: 'Success'}
+  },
+  handler: ctrl.putCommentsWithId
+})
+
+const deleteCommentsWithId = new ApiRouter({
+  name: ':id',
+  paths: ['common/IdPath'],
+  method: 'delete',
+  summary: 'comments 삭제',
+  tags: ['Comment'],
+  responses: {
+    200: {description: 'Success'}
+  },
+  handler: ctrl.deleteCommentsWithId
+})
+
+export {postComments, getComments, putCommentsWithId, deleteCommentsWithId}
