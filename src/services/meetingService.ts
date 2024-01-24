d1897f9e61bea6169f944576378db9b1126f8aca
devHDK <hyundo91082@gmail.com>
2024-01-24 17:30:59 +0900

    welcomeThreadGallery nullable 처리

--- src/services/meetingService.ts
+++ src/services/meetingService.ts
@@ -40,7 +40,7 @@ async function create(options: {trainerId: number; userId: number; startTime: st
       {
         title: `${user.nickname.substring(1)}님 안녕하세요!`,
         content: trainerThread.welcomeThreadContent,
-        gallery: trainerThread.welcomeThreadGallery ?? JSON.stringify(trainerThread.welcomeThreadGallery),
+        gallery: trainerThread.welcomeThreadGallery ? JSON.stringify(trainerThread.welcomeThreadGallery) : null,
         trainerId,
         userId,
         type: 'general',
--- src/services/threadService.ts
+++ src/services/threadService.ts
@@ -35,7 +35,7 @@ async function create(options: IThreadCreateOne): Promise<IThreadCreatedId> {
           {
             title: `${user.nickname.substring(1)}님 안녕하세요!`,
             content: trainerThread.welcomeThreadContent,
-            gallery: trainerThread.welcomeThreadGallery ?? JSON.stringify(trainerThread.welcomeThreadGallery),
+            gallery: trainerThread.welcomeThreadGallery ? JSON.stringify(trainerThread.welcomeThreadGallery) : null,
             trainerId,
             userId,
             type: 'general',
